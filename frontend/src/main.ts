820f7906a264b242e199f4c9646ae895a8236a6f
Mu_Cloud <Mu_Cloud@outlook.de>
2025-05-15 17:05:38 +0800

    [FrontEnd] Import ElementPlus | Import Poppins Font

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(ElementPlus, { size: 'small', zIndex: 3000})
+    .mount('#app')
