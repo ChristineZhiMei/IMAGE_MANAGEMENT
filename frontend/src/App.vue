78e03967dc28395b087ea78d38018af73d4f5bab
ChristineZhiMei <2977797064@qq.com>
2025-05-21 17:55:22 +0800

    feat:

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,41 +9,41 @@ const toggleDark = useToggle(isDark);
 </script>
 
 <template>
-  <el-container >
-    <el-header class="main-nav switch-color switch-color-transition" height="64px">
-      <el-row class="h-full" align="middle">
-        <el-icon size="64px" class="p-4.5" @click.prevent="store.changeMenuCollapse()">
-          <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M4 6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H5C4.44772 7 4 6.55228 4 6Z" fill="currentColor"/>
-            <path d="M4 18C4 17.4477 4.44772 17 5 17H19C19.5523 17 20 17.4477 20 18C20 18.5523 19.5523 19 19 19H5C4.44772 19 4 18.5523 4 18Z" fill="currentColor"/>
-            <path d="M5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H13C13.5523 13 14 12.5523 14 12C14 11.4477 13.5523 11 13 11H5Z" fill="currentColor"/>
-          </svg>
-        </el-icon>
-        <span class="text-xl select-none">Image Management<sup class="ml-0.5">DEV.1</sup></span>
-        <button @click="toggleDark()" class="cursor-pointer border-b-2 border-dotted">
-          <span class="ml-2 text-xl">{{ isDark ? 'Dark Edition' : 'Light Edition' }}</span>
-        </button>
-      </el-row>
-    </el-header>
-    <el-container class="main-view">
-      <el-menu
-        class="left-menu switch-color-transition switch-color"
-        :collapse="store.isMenuCollapse"
-      >
-        <el-menu-item>
-          <el-icon size="32">
-            <svg class="stroke-2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" stroke="currentColor"/>
+    <el-container >
+      <el-header class="main-nav switch-color" height="64px">
+        <el-row class="h-full" align="middle">
+          <el-icon size="64px" class="p-4.5" @click.prevent="store.changeMenuCollapse()">
+            <svg fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M4 6C4 5.44772 4.44772 5 5 5H19C19.5523 5 20 5.44772 20 6C20 6.55228 19.5523 7 19 7H5C4.44772 7 4 6.55228 4 6Z" fill="currentColor"/>
+              <path d="M4 18C4 17.4477 4.44772 17 5 17H19C19.5523 17 20 17.4477 20 18C20 18.5523 19.5523 19 19 19H5C4.44772 19 4 18.5523 4 18Z" fill="currentColor"/>
+              <path d="M5 11C4.44772 11 4 11.4477 4 12C4 12.5523 4.44772 13 5 13H13C13.5523 13 14 12.5523 14 12C14 11.4477 13.5523 11 13 11H5Z" fill="currentColor"/>
             </svg>
           </el-icon>
-          <template #title><span class="mx-4">Overview</span></template>
-        </el-menu-item>
-      </el-menu>
-      <el-main class="switch-color-transition switch-color-light">
+          <span class="text-xl select-none">Image Management<sup class="ml-0.5">DEV.1</sup></span>
+          <button @click="toggleDark()" class="cursor-pointer border-b-2 border-dotted">
+            <span class="ml-2 text-xl">{{ isDark ? 'Dark Edition' : 'Light Edition' }}</span>
+          </button>
+        </el-row>
+      </el-header>
+      <el-container class="main-view">
+        <el-menu
+          class="left-menu switch-color"
+          :collapse="store.isMenuCollapse"
+        >
+          <el-menu-item>
+            <el-icon size="32">
+              <svg class="stroke-2" fill="none" stroke-linecap="round" stroke-linejoin="round" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z" stroke="currentColor"/>
+              </svg>
+            </el-icon>
+            <template #title><span class="mx-4">Overview</span></template>
+          </el-menu-item>
+        </el-menu>
+        <el-main class=" switch-color-light">
 
-      </el-main>
+        </el-main>
+      </el-container>
     </el-container>
-  </el-container>
 </template>
 
 <style scoped>
@@ -54,11 +54,19 @@ const toggleDark = useToggle(isDark);
     width: 200px;
     flex-shrink: 0 !important;
 }
-.switch-color-transition{
-  transition: background-color .4s ease-in-out ,
-                color .4s ease-in-out !important;
 
+
+.main-nav{
+    padding: 0;
+    border-bottom: gray 1px solid;
 }
+el-container{
+    height: 100lvh;
+}
+.main-view{
+    height: calc(100dvh - 60px);
+}
+
 .switch-color{
   color:var(--front-view-color-light);
   background-color: var(--back-view-color);
@@ -67,15 +75,5 @@ const toggleDark = useToggle(isDark);
   color:var(--front-view-color-light);
   background-color: var(--back-view-color-light);
 }
-.main-nav{
-    padding: 0;
-    border-bottom: gray 1px solid;
 
-}
-el-container{
-    height: 100lvh;
-}
-.main-view{
-    height: calc(100dvh - 60px);
-}
 </style>
